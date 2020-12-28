359f6de869345aac8a7e9d5670f95fc1edc3547f
wdy165 <wdy165@naver.com>
2020-12-28 11:38:38 +0900

    주어진 값의 최소, 최대값 구하기(배열을 정렬한 후 출력[시간복잡도 느림])

---
+++ 10818_2/src/Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+		int A[] = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		
+		System.out.print(A[0] + " " + A[N-1]);
+	}
+}
