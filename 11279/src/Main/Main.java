4fcdebb0ebcbf818cfda783983c50c5b76e38220
wdy165 <wdy165@naver.com>
2021-01-18 21:29:30 +0900

    PriorityQueue 최대값 문제

---
+++ 11279/src/Main/Main.java
@@ -0,0 +1,30 @@
+package Main;
+import java.util.PriorityQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+public class Main {
+
+	public static void main(String[] args)throws NumberFormatException, IOException {
+		PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] x = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			x[i] = Integer.parseInt(br.readLine());
+			if(x[i] == 0) {
+				if(queue.isEmpty()) {
+					System.out.println(0);
+				}else {
+					System.out.println(queue.poll());
+				}
+			}else {
+				queue.add(x[i]);
+			}
+		}
+
+	}
+
+}
